d483197e5ea71914d6963f6ce2c9de55a9eedfd1
Yavuz Selim Yayla <yavuzyayla98@gmail.com>
2022-01-04 11:51:23 +0300

    Added smooth camera follow and changed obstacle materials

--- Assets/scripts/FollowPlayer.cs
+++ Assets/scripts/FollowPlayer.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour {
+public class FollowPlayer : MonoBehaviour
+{
 
-    //current player (square)
-    private GameObject player;
-
-    //offset of the camera from the player
-    public Vector3 cameraPlayerOffset;
+    
+    private GameObject player;              //current player (square)
+    public Vector3 cameraPlayerOffset;      // Offset of the camera from the player
+    public float lerpTime = 0.1f;
 
     private void Start()
     {
-        player = GameObject.FindWithTag("Player"); 
+        player = GameObject.FindWithTag("Player");
     }
 
     //On update, set the camera position to be position of the player
-    void Update () {
+    void Update()
+    {
 
         Transform playerPosition = player.transform;
         //position the camera the same as the player but with an offset
-        transform.position = playerPosition.position + cameraPlayerOffset;
+        transform.position = Vector3.Lerp(
+            transform.position,
+            playerPosition.position + cameraPlayerOffset,
+            0.1f);
     }
 }
