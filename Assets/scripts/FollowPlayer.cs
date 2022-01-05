06d18130118c0cd0f93bf301566751b163db3635
Yavuz Selim Yayla <yavuzyayla98@gmail.com>
2022-01-05 10:20:47 +0300

    Fixed camera smoothing and changed ultracoin color

--- Assets/scripts/FollowPlayer.cs
+++ Assets/scripts/FollowPlayer.cs
@@ -14,14 +14,15 @@ public class FollowPlayer : MonoBehaviour
     }
 
     //On update, set the camera position to be position of the player
-    void Update()
+    void FixedUpdate()
     {
 
         Transform playerPosition = player.transform;
         //position the camera the same as the player but with an offset
+        Vector3 trgPos = playerPosition.position + cameraPlayerOffset;
         transform.position = Vector3.Lerp(
             transform.position,
-            playerPosition.position + cameraPlayerOffset,
+            new Vector3(trgPos.x, cameraPlayerOffset.y, trgPos.z),
             0.1f);
     }
 }
